05616c429e1b634114801b0c96c86b66f265e186
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-05-21 09:21:06 +0200

    hoofdstuk 5

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -19,18 +19,15 @@ public class BedrijfLauncher {
         afdelingen[2] = new Afdeling("Management", "Almere");
         afdelingen[3] = new Afdeling("Documentatie", "Gouda");
 
-        Afdeling[] afdelingen2 = {
-                new Afdeling("Uitvoering", "Hilversum"),
-                new Afdeling("Support", "Amsterdam"),
-                new Afdeling("Management", "Almere"),
-                new Afdeling("Documentatie", "Gouda")
-        };
-
         Werknemer baas = new Werknemer("Mark", "Den Haag", afdelingen[2], 10000);
         Werknemer medewerker = new Werknemer("Caroline", "Delft", afdelingen[1], 4000);
         ZZPer assistent = new ZZPer("Klaas", "Diemen", afdelingen[3], 50.00);
+        ZZPer projectleider = new ZZPer("Ronald", "Zaandam", afdelingen[0], 80);
 
         assistent.huurIn(160);
+        projectleider.huurIn(320);
+
+        Persoon[] personen = {baas, medewerker, assistent, projectleider};
 
         System.out.printf("Het aantal personen in het bedrijf is %d\n", Persoon.getAantalPersonen());
         System.out.println(baas);
@@ -38,8 +35,14 @@ public class BedrijfLauncher {
         System.out.println(assistent);
 
         System.out.println();
-        System.out.printf("%s verdient %.2f per jaar\n", baas.getNaam(), baas.berekenJaarinkomen());
-        System.out.printf("%s verdient %.2f per jaar\n", medewerker.getNaam(), medewerker.berekenJaarinkomen());
-        System.out.printf("%s verdient %.2f per jaar\n", assistent.getNaam(), assistent.berekenJaarinkomen());
-}
+        for (Persoon persoon : personen) {
+            toonJaarinkomen(persoon);
+        }
+    }
+
+    public static void toonJaarinkomen(Persoon persoon) {
+        System.out.printf("%s verdient %.2f per jaar\n", persoon.getNaam(), persoon.berekenJaarinkomen());
+    }
+
+
 }
